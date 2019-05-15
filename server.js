c3af5626d91540f52c8fbea7dc47c0a7dc914379
LD <LDinho@users.noreply.github.com>
2019-05-15 00:52:03 -0400

    add logger middleware

--- server.js
+++ server.js
@@ -2,25 +2,28 @@ const express = require('express');
 
 const server = express();
 
-
 // import routers
 const userRouter = require("./users/userRouter");
+
+
+//GLOBAL Middleware
 
 // built in middleware
 server.use(express.json()); // parses body
 
+//custom middleware
+server.use(logger);
+
+function logger(req, res, next) {
+  console.log(`${new Date().toISOString()}: ${req.method} to ${req.url}`);
+  next();
+}
+
+// routers - users
+server.use("/api/users", userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
-
-//custom middleware
-
-function logger(req, res, next) {
-
-};
-
-// routers - users
-server.use("/api/users", userRouter);
 
 module.exports = server;
