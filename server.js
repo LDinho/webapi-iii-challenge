a7be9528cb20a4dba19ab2cc49eafc90eeda0afa
LD <LDinho@users.noreply.github.com>
2019-05-15 20:33:26 -0400

    add validateUserId middleware

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ server.use(express.json()); // parses body
 //custom middleware
 server.use(logger);
 
-function logger(req, res, next) {
-  console.log(`${new Date().toISOString()}: ${req.method} to ${req.url}`);
-  next();
-}
-
 // routers - users
 server.use("/api/users", userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
+function logger(req, res, next) {
+  console.log(`${new Date().toISOString()}: ${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const db = require('./userDb')
+
+const { get, getById, getUserPosts, insert, remove, update } = db;
+
 router.post('/', (req, res) => {
 
 });
@@ -32,9 +36,25 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const { id } = req.params;
 
-};
+  try {
+    const user = await getById(id);
+
+    if (user) {
+      req.user = user;
+      next();
+
+    } else {
+      res.status(404).json({message: 'invalid user id'})
+    }
+  }
+  catch (err) {
+    res.status(500).json({message: 'server unable to process your request'})
+  }
+
+}
 
 function validateUser(req, res, next) {
 
